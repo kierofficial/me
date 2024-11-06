816610f9624cb369f81f3fd6f5644b304bddc98b
kierofficial <kirochan.kd@gmail.com>
2024-11-06 12:51:55 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "dist",
+  base: "/me/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
